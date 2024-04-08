d6657d331e988cc22daae87f6877d713db48dcf7
Haggai Lerman <haguyle@gmail.com>
2024-04-08 16:13:20 -0400

    added ec2-module and aws elastic ip resource block for bustion host

---
+++ compute.tf
@@ -0,0 +1,25 @@
+# AWS EC2 Instance Terraform Module
+# Bastion Host - EC2 Instance that will be created in VPC Public Subnet
+module "ec2_public" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~>5.6.1"
+
+  name          = "${local.name}-BastionHost"
+  ami           = data.aws_ami.amzlinux2.id
+  instance_type = var.instance_type
+  key_name      = var.instance_keypair
+  #monitoring             = true
+  subnet_id              = module.vpc.public_subnets[0]
+  vpc_security_group_ids = [module.public_bastion_sg.security_group_id]
+
+  tags = local.common_tags
+}
+
+# Create Elastic IP for Bastion Host
+# Resource - depends_on Meta-Argument
+resource "aws_eip" "bastion_eip" {
+  depends_on = [module.ec2_public, module.vpc]
+  instance   = module.ec2_public.id
+  domain     = "vpc"
+  tags       = local.common_tags
+}
